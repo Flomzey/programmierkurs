a6bf425d66455d305159b2596a0428b5c1455519
Florian Unger <e12416316@student.tuwien.ac.at>
2025-05-24 15:54:22 +0200

    test

--- lesson1.py
+++ lesson1.py
@@ -4,10 +4,9 @@ z = "mars"
 a = "jupiter"
 b = "merkur"
 
-#   1 2 3 4 5 3
+#   1 2 3 4 5
 #   0 1 2 3 4
 c = x,y,z,a,b
 
 for i in range(5):
     print(c[i])
-
---
+++ lesson2.py
@@ -0,0 +1,19 @@
+# Ein Billa hat verschiedene Gänge ein produkt ist in einem Gang
+from traceback import format_list
+
+p0 = "Apfel"
+p1 = "Banane"
+
+
+
+g0 = "Obst"
+g1 = "Fleisch"
+g2 = "Tiefkühlwahren"
+g3 = "Backwahren"
+g4 = "Kosmetik"
+g5 = "Getränke"
+g6 = "Gemüse"
+
+billa = g0, g1, g2, g3, g4, g5, g6
+
+print(billa)
